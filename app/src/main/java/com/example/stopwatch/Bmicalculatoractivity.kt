01d86dd7893c0cc556311bd391a5865b91f6aa38
jud-ie19 <owuorakinyijudith@gmail.com>
2022-05-05 20:25:57 +0300

    first commit

---
+++ app/src/main/java/com/example/stopwatch/Bmicalculatoractivity.kt
@@ -0,0 +1,11 @@
+package com.example.stopwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Bmicalculatoractivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bmicalculatoractivity)
+    }
+}
