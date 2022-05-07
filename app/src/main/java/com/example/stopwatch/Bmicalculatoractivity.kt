968cf71e04387da9af0543e546f612e2b9b2ce02
jud-ie19 <owuorakinyijudith@gmail.com>
2022-05-07 16:51:03 +0300

    add changes

--- app/src/main/java/com/example/stopwatch/Bmicalculatoractivity.kt
+++ app/src/main/java/com/example/stopwatch/Bmicalculatoractivity.kt
@@ -1,11 +1,21 @@
 package com.example.stopwatch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
-class Bmicalculatoractivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bmicalculatoractivity)
+
+    class Bmicalculatoractivity : AppCompatActivity() {
+        lateinit var btntransaction: Button
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_bmicalculatoractivity)
+            btntransaction=findViewById(R.id.btntransaction)
+            btntransaction.setOnClickListener {
+                val intent= Intent(this ,TransactionAcitivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
-}
+
